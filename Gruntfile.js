f2cebef291eaaa9cca18d366bceec14829209b94
Andre Calvo <andre.calvo@15gifts.com>
2014-09-13 16:49:59 +0100

    Moving the minified JS in to a separate directory to prevent a loop of watching/compiling

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function(grunt) {
     uglify: {
       my_target: {
         files: {
-          'js/main.min.js': ['js/main.js']
+          'js-min/main.min.js': ['js/main.js']
         },
       },
     },
